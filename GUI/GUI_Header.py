95051de4852ddd3d7210ad5e87898e4ff7d341de
Braeden Huot <59823288+braeden87@users.noreply.github.com>
2021-10-28 14:28:06 -0400

    Update GUI_Header.py

--- GUI/GUI_Header.py
+++ GUI/GUI_Header.py
@@ -2,14 +2,19 @@ import pygame
 import constants as const
 
 class header_box:
-
+######################################################################
+# Constructor
+######################################################################
     def __init__(self, x, y, width, height, text):
         self.header = pygame.Rect(x, y, width, height)
         self.text = text    
         self.color = const.BACKGROUND_COLOR
         self.text_surface = const.FONT.render(text, True, const.TEXT_COLOR)
-
-
+######################################################################
+# Draw Method
+#
+# This method is used to add the Header to the Screen / Window 
+######################################################################
     def draw(self, screen):
        #Text 
         screen.blit(self.text_surface, (self.header.x + 5, self.header.y + 5))
